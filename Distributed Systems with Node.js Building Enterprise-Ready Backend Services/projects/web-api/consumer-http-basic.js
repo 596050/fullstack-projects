c690bb0d7e54cb166aec4806c33316a2adf16ef1
Shrouded Shrew <shroudedshrew@gmail.com>
2022-02-14 21:02:09 +0000

    Add cors for posts and comments apis

--- Distributed Systems with Node.js Building Enterprise-Ready Backend Services/projects/web-api/consumer-http-basic.js
+++ Distributed Systems with Node.js Building Enterprise-Ready Backend Services/projects/web-api/consumer-http-basic.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 const server = require('fastify')();
 const fetch = require('node-fetch');
+
 const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET || 'localhost:4000';
+
 server.get('/', async () => {
   const req = await fetch(`http://${TARGET}/recipes/42`);
   const producer_data = await req.json();
@@ -12,6 +14,7 @@ server.get('/', async () => {
     producer_data,
   };
 });
+
 server.listen(PORT, HOST, () => {
   console.log(`Consumer running at http://${HOST}:${PORT}/`);
 });

f4d559a6a3a9e8cf58fb60f32d81905710aa401f
Shrouded Shrew <shroudedshrew@gmail.com>
2022-02-16 15:21:59 +0000

    Add docker, events and moderation service

--- Microservices-with-Node-JS-and-React/blog/packages/services/comments/index.js
+++ Microservices-with-Node-JS-and-React/blog/packages/services/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +10,7 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:postId/comments', (req, res) => {
+app.get('/posts/:postId/comments', async (req, res) => {
   res.send(commentsByPostId[req.params.postId] || []);
 });
 
@@ -19,9 +20,41 @@ app.post('/posts/:postId/comments', (req, res) => {
   const { content } = req.body;
   const comments = commentsByPostId[postId] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
   commentsByPostId[postId] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId,
+      status: 'pending',
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find(comment => comment.id === id);
+    comment.status = data.status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        postId,
+        id,
+        status,
+        content,
+      },
+    });
+  }
+  res.send({});
 });
 
 app.listen(4001, () => {

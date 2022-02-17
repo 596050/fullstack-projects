4db320184476f085842d1ae72c058edb8211b284
Shrouded Shrew <shroudedshrew@gmail.com>
2022-02-17 00:29:01 +0000

    Add routes

--- Microservices-with-Node-JS-and-React/ticketing/auth/src/index.ts
+++ Microservices-with-Node-JS-and-React/ticketing/auth/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+app.use(currentUserRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
